9225b373b84379c674fe4c7cfe2ac4b6f132350a
c0 <122c597083bd438b7f6d72af75d0259488996477@cipher0.dev>
2026-04-05 20:21:04 -0600

    Initial work on transpose cache memory optimizations

--- cache/trans.c
+++ cache/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,44 +6,101 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
-#include <stdio.h>
+ */
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+// #define printf_debug(...)
+#define printf_debug(...) printf(__VA_ARGS__)
+
+void print_matrix(int M, int N, int A[M][N]) {
+  printf_debug("rows: %d, cols: %d:\n", M, N);
+  for (auto i = 0; i < M; i++) {
+    for (auto j = 0; j < N; j++) {
+      printf_debug(" %2d", A[i][j]);
+    }
+    printf_debug("\n");
+  }
+  printf_debug("\n");
+}
+
+int min(int a, int b) { return (a < b) ? a : b; }
+int max(int a, int b) { return (a < b) ? b : a; }
+
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+  // initialize for debugging
+  // auto num = 0;
+  // for (auto i = 0; i < N; i++) {
+  //   for (auto j = 0; j < M; j++) {
+  //     A[i][j] = ++num;
+  //     printf_debug("A[%d][%d]: %d\n", i, j, A[i][j]);
+  //   }
+  // }
+  // for (auto i = 0; i < M; i++) {
+  //   for (auto j = 0; j < N; j++) {
+  //     B[i][j] = -1;
+  //   }
+  // }
+
+#define BATCH_COMPONENTS 4
+#define MIN_BATCH_SIZE 2
+
+  auto a_batch_rows = min(max(N / BATCH_COMPONENTS, MIN_BATCH_SIZE), N);
+  auto a_batch_cols = min(max(M / BATCH_COMPONENTS, MIN_BATCH_SIZE), M);
+
+  for (auto i = 0; i < N / a_batch_rows; i++) {
+    for (auto j = 0; j < M / a_batch_cols; j++) {
+      printf_debug("batch: [%d][%d]\n", i, j);
+      for (auto k = 0; k < a_batch_rows; k++) {
+        auto a_row = i * a_batch_rows + k;
+        auto b_col = a_row;
+        for (auto l = 0; l < a_batch_cols; l++) {
+          auto a_col = j * a_batch_cols + l;
+          auto b_row = a_col;
+
+          printf_debug("moving: A[%d][%d] to B[%d][%d]: %d\n", a_row, a_col,
+                       b_row, b_col, A[a_row][a_col]);
+          B[b_row][b_col] = A[a_row][a_col];
+        }
+      }
+    }
+
+    // TODO: implement remaining cols/rows
+  }
+
+  print_matrix(N, M, A);
+  print_matrix(M, N, B);
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
 
-/* 
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j, tmp;
+void trans(int M, int N, int A[N][M], int B[M][N]) {
+  int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
-
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      tmp = A[i][j];
+      B[j][i] = tmp;
+    }
+  }
 }
 
 /*
@@ -53,32 +110,28 @@ void trans(int M, int N, int A[N][M], int B[M][N])
  *     performance. This is a handy way to experiment with different
  *     transpose strategies.
  */
-void registerFunctions()
-{
-    /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+void registerFunctions() {
+  /* Register your solution function */
+  registerTransFunction(transpose_submit, transpose_submit_desc);
 
-    /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+  /* Register any additional transpose functions */
+  registerTransFunction(trans, trans_desc);
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
  */
-int is_transpose(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j;
+int is_transpose(int M, int N, int A[N][M], int B[M][N]) {
+  int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
-                return 0;
-            }
-        }
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; ++j) {
+      if (A[i][j] != B[j][i]) {
+        return 0;
+      }
     }
-    return 1;
+  }
+  return 1;
 }
-
